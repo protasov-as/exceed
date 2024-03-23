cb4cfcb2059e1d9638e88368e92564c20b19454f
Aleksey Protasov <aprotasov@axbit.ru>
2024-03-23 11:22:20 +0300

    Initial commit

---
+++ modules/core/test/com/company/exceed/ExceedTestContainer.java
@@ -0,0 +1,58 @@
+package com.company.exceed;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ExceedTestContainer extends TestContainer {
+
+    public ExceedTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/company/exceed/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/company/exceed/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends ExceedTestContainer {
+
+        public static final ExceedTestContainer.Common INSTANCE = new ExceedTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
